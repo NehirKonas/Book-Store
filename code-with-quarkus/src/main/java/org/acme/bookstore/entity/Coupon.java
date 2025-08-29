451140b78b765937ba93f78a7f7d69e9cd6c0898
Aslı Kanber <145851208+asliKanber@users.noreply.github.com>
2025-08-29 10:39:26 +0300

    coupon and final
    coupon eklendi final yaptım customeri

--- code-with-quarkus/src/main/java/org/acme/bookstore/entity/Coupon.java
+++ code-with-quarkus/src/main/java/org/acme/bookstore/entity/Coupon.java
@@ -1,5 +1,65 @@
 package org.acme.bookstore.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "coupon")
 public class Coupon {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cupon_id;
+
+    @Column(name = "discount" ,nullable= false)
+    private int discount;
+
+    @Column(name = "exp_date")
+    private LocalDate expDate;
+
+    @Column(name="isUsed")
+    private boolean isUsed = false;
+
+    //getters and setters
+    public Long getCupon_id() {
+        return cupon_id;
+    }
+
+    public void setCupon_id(Long cupon_id) {
+        this.cupon_id = cupon_id;
+    }
+
+    // Getter and Setter for discount
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    // Getter and Setter for expDate
+    public LocalDate getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(LocalDate expDate) {
+        this.expDate = expDate;
+    }
+
+    // Getter and Setter for isUsed
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
 }
--- code-with-quarkus/src/main/java/org/acme/bookstore/entity/Customer.java
+++ code-with-quarkus/src/main/java/org/acme/bookstore/entity/Customer.java
@@ -1,11 +1,17 @@
 package org.acme.bookstore.entity;
 
-import jakarta.persistence.*;
 import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customers")
-public class Customer {
+public final class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
